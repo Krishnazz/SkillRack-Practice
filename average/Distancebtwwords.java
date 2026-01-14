0e5b69d0c3dd0a1906aafad114993370b1340a87
Krishnazz <talktokrishna06@gmail.com>
2026-01-14 23:23:14 +0530

    Add Distancebtwwords.java to calculate distance between words in a string

---
+++ average/Distancebtwwords.java
@@ -0,0 +1,29 @@
+package average;
+import java.util.*;
+import java.io.*;
+public class Distancebtwwords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		String str1=s.nextLine();
+		String str2=s.nextLine();
+		String[] a=str.split("\\s");
+		int start=0;
+        int end=0;
+        int min=0;
+		for(int i=0;i<a.length;i++) {
+			
+				if(a[i].equals(str1))
+					start=i;
+				if(a[i].equals(str2))
+					end=i;
+				if(end-start<=min)
+					min=end-start;
+		}
+		System.out.print(min);
+				
+	}
+
+}
