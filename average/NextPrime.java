3e1aae9630f13dd97fb372d7e8702e60e343b028
Krishnazz <talktokrishna06@gmail.com>
2026-02-01 20:27:35 +0530

    Add NextPrime.java to implement logic for finding the next prime number

---
+++ average/NextPrime.java
@@ -0,0 +1,23 @@
+package easy;
+import java.io.*;
+import java.util.*;
+public class NextPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt(),count=0;
+		for(int i=1;i<=n;i++)
+		{
+		  if(n%i==0)
+		  {
+		     count++;
+		    // break;
+		  }
+		}
+	if(count==2)
+		System.out.print(n);
+	
+
+}
+}
