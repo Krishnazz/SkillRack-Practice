bb875f8be4d77d4f4bb4fcc6db17391d4c1846a6
Krishnazz <talktokrishna06@gmail.com>
2026-04-16 20:05:36 +0530

    Add reverseRows.java to reverse the order of rows in a 2D array

---
+++ average/reverseRows.java
@@ -0,0 +1,27 @@
+package average;
+import java.io.*;
+import java.util.*;
+public class reverseRows {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int r=s.nextInt();
+		int c=s.nextInt();
+		int[][] a=new int[r][c];
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				a[i][j]=s.nextInt();
+			}
+		}
+		for(int i=r-1;i>=0;i--) {
+			for(int j=0;j<c;j++) {
+				System.out.print(a[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+}
