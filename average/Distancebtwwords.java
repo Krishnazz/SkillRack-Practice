968d22091c5bb98beba8b6d632dac6d6ee411a36
Krishnazz <talktokrishna06@gmail.com>
2026-04-21 23:27:18 +0530

    Remove autogenerated comment in Distancebtwwords.java

--- average/Distancebtwwords.java
+++ average/Distancebtwwords.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Distancebtwwords {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
 		String str=s.nextLine();
 		String str1=s.nextLine();
