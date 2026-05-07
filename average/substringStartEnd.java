d63b1c05a7ef8722428195cce0fbdd4f1ea3c2d5
Krishnazz <talktokrishna06@gmail.com>
2026-05-07 23:03:29 +0530

    Add substringStartEnd class to demonstrate substring manipulation

---
+++ average/substringStartEnd.java
@@ -0,0 +1,25 @@
+package average;
+
+import java.util.Scanner;
+
+
+public class substringStartEnd {
+    public static void main(String[] args) {
+    //    String str="Important";
+    //    int start=2;
+    //    int end=4;
+        String str="think";
+        int start=3;
+        int end=1;
+       if(start<=end){
+        System.out.println(str.substring(start,end+1));
+       }
+       else{
+        String str1=str.substring(start);
+        String str2=str.substring(0,end+1);
+        System.out.println(str1+str2);
+       }
+    
+
+    }
+}
