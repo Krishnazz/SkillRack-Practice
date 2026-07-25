431bbb0f23b2d170a01ae2c4c8cf0fb3686a1315
Krishnazz <talktokrishna06@gmail.com>
2026-07-25 12:36:28 +0530

    Refactor Distancebtwwords.java to improve distance calculation logic and handle identical words

--- average/Distancebtwwords.java
+++ average/Distancebtwwords.java
@@ -2,26 +2,52 @@ package average;
 import java.util.*;
 public class Distancebtwwords {
 
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		String str=s.nextLine();
-		String str1=s.nextLine();
-		String str2=s.nextLine();
-		String[] a=str.split("\\s");
-		int start=0;
-        int end=0;
-        int min=0;
-		for(int i=0;i<a.length;i++) {
-			
-				if(a[i].equals(str1))
-					start=i;
-				if(a[i].equals(str2))
-					end=i;
-				if(end-start<=min)
-					min=end-start;
-		}
-		System.out.print(min);
-				
-	}
+    public static void main(String[] args) {
 
+        String str = "practice makes perfect practice coding practice";
+        String word1 = "practice";
+        String word2 = "practice";
+
+        String[] words = str.split("\\s+");
+
+        int minDistance = Integer.MAX_VALUE;
+
+        if (word1.equals(word2)) {
+
+            int previous = -1;
+
+            for (int i = 0; i < words.length; i++) {
+
+                if (words[i].equals(word1)) {
+
+                    if (previous != -1) {
+                        minDistance = Math.min(minDistance,
+                                i - previous);
+                    }
+
+                    previous = i;
+                }
+            }
+
+        } else {
+
+            int pos1 = -1;
+            int pos2 = -1;
+
+            for (int i = 0; i < words.length; i++) {
+
+                if (words[i].equals(word1))
+                    pos1 = i;
+
+                if (words[i].equals(word2))
+                    pos2 = i;
+
+                if (pos1 != -1 && pos2 != -1)
+                    minDistance = Math.min(minDistance,
+                            Math.abs(pos1 - pos2));
+            }
+        }
+
+         System.out.println(minDistance);
+    }
 }
